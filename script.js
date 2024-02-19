8769969895a16ee61db301adeb2cb3d17ccea631
Nebuchadnezzar <110613468+Nebuchadnazzer@users.noreply.github.com>
2024-02-19 16:36:12 +0800

    Computer Choice returns 0 to 2, Player Choice converted to 0, 1 or 2

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 //Computer Selection
-    //Generate a random number from 1 to 3
-    //Numbers 1 to 3 are assigned to rock, paper or scissors
+    //Generate a random number from 0 to 2
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
-    let computerSelection;
-
-    switch (randomNumber) {
-        case 1:
-            computerSelection = "rock"
-            break;
-        case 2:
-            computerSelection = "paper"
-            break;
-        case 3:
-            computerSelection = "scissors"
-            break;
-    }
-    return computerSelection;
+    const computerChoice = Math.floor(Math.random() * 3);
+    return computerChoice;
     }
 
 //User Selection
+    //Assume user will only type rock, paper or scissors
     //Prompts user to type a selection
-    //Users typed selection is coverted to lower strings for consistency
+    //User selection is converted to 0, 1 or 2
 function getPlayerChoice() {
-    let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-    return playerSelection;
+    let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+    switch (playerInput) {
+        case 'rock':
+            playerChoice = 0
+            break;
+        case 'paper':
+            playerChoice = 1
+            break;
+        case 'scissors':
+            playerChoice = 2
+            break;
+    }
+    return playerChoice;
 }
+
+//Get both values and evaluate final result
+function playRound() {
+    let player1 = getPlayerChoice()
+    let player2 = getComputerChoice()
+    console.log(player1)
+    console.log(player2)
+}
+
+playRound();
