4c9a56968c0740198bb314890d873459b31ac063
Nebuchadnezzar <110613468+Nebuchadnazzer@users.noreply.github.com>
2024-02-20 20:53:47 +0800

    Created playGame function

--- script.js
+++ script.js
@@ -44,29 +44,55 @@ function getChoiceName (selection) {
 //Get both values and evaluate final result
 function playRound() {
     //Get players selection (0, 1 or 2)
-    const player1 = getPlayerChoice()
-    const player2 = getComputerChoice()
+    const player1 = getPlayerChoice();
+    const player2 = getComputerChoice();
     //Translate players selection (Rock, Paper or Scissors)
     let playerSelection = getChoiceName(player1);
     let computerSelection = getChoiceName(player2);
+    let player1Score = 0;
+    let player2Score = 0;
+    let finalResult;
 
     //Equation `(player2 - player1 + 3)` created from rock paper scissors truth table
     //Winner is decided on difference of equation, hence adding `%3` to equation
     const result = (player2 - player1 + 3) % 3
 
     //Show who won by switch statement
-    let finalResult
     switch (result) {
         case 0:
             finalResult = "Draw"
             break;
         case 1:
             finalResult = (`Player 2 Wins! ${computerSelection} beats ${playerSelection}!`)
+            player2Score += 1
             break;
         case 2:
             finalResult = (`Player 1 Wins! ${playerSelection} beats ${computerSelection}!`)
+            player1Score += 1
             break;
     }
-    console.log(finalResult);}
+    console.log(finalResult);
+    return [player1Score, player2Score]
+}
 
-playRound()
+//Add counter
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+
+    for (let i = 0; i < 5; i++) {
+        round = playRound()
+        humanScore += round[0]
+        computerScore += round[1]
+      }
+
+    console.log(`
+    Human: ${humanScore} 
+    Computer: ${computerScore}`)
+}
+
+playGame();
+//The game works
+//But it keeps propmting the user for their selection 
+//without being shown the results on the last round before being prompted to start another round
+//Player should have the option to start the round
