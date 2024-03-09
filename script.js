ce7c142bf6cdaa7be69bfc8f5d2711b8d7b95dee
Nebuchadnezzar <110613468+Nebuchadnazzer@users.noreply.github.com>
2024-03-10 04:30:57 +0800

    Created seperate styles.css for ui elements

--- script.js
+++ script.js
@@ -9,20 +9,15 @@ function getComputerChoice() {
     //Prompts user to type a selection
         //Assume user will only type rock, paper or scissors
     //User selection is converted to 0, 1 or 2
-function getPlayerChoice() {
-    let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+function getPlayerChoice(playerChoice) {
     switch (playerChoice) {
         case 'rock':
-            playerChoice = 0
-            break;
+            return 0;
         case 'paper':
-            playerChoice = 1
-            break;
+            return 1;
         case 'scissors':
-            playerChoice = 2
-            break;
+            return 2;
     }
-    return playerChoice;
 }
 
 //Translate number selection to string definition
@@ -41,10 +36,8 @@ function getChoiceName (choiceName) {
     return choiceName
 }
 
-//Get both values and evaluate final result
-function playRound() {
-    //Get players selection (0, 1 or 2)
-    const player1 = getPlayerChoice();
+function playRound(player1) {
+    //Get computers selection (0, 1 or 2)
     const player2 = getComputerChoice();
     //Translate players selection (Rock, Paper or Scissors)
     let playerSelection = getChoiceName(player1);
@@ -56,43 +49,96 @@ function playRound() {
     //Equation `(player2 - player1 + 3)` created from rock paper scissors truth table
     //Winner is decided on difference of equation, hence adding `%3` to equation
     const result = (player2 - player1 + 3) % 3
-
     //Show who won by switch statement
     switch (result) {
         case 0:
-            finalResult = "Draw"
+            finalResult = "It's a Draw!"
             break;
         case 1:
-            finalResult = (`Player 2 Wins! ${computerSelection} beats ${playerSelection}!`)
+            finalResult = (`${computerSelection} beats ${playerSelection}!`)
             player2Score += 1
             break;
         case 2:
-            finalResult = (`Player 1 Wins! ${playerSelection} beats ${computerSelection}!`)
+            finalResult = (`${playerSelection} beats ${computerSelection}!`)
             player1Score += 1
             break;
     }
-    console.log(finalResult);
-    return [player1Score, player2Score]
+    const resultDiv = document.querySelector('#results');
+    resultDiv.innerText = finalResult;
+    return [player1Score, player2Score];
 }
 
-//Add counter
-function playGame() {
-    let playerScore = 0
-    let computerScore = 0
+let totalPlayer1Score = 0;
+let totalPlayer2Score = 0;
+const winningScore = 5;
 
-    for (let i = 0; i < 5; i++) {
-        round = playRound()
-        playerScore += round[0]
-        computerScore += round[1]
-      }
-
-    console.log(`
-    Player: ${playerScore} 
-    Computer: ${computerScore}`)
+function updateScores(player1Score = 0, player2Score = 0) {
+    totalPlayer1Score += player1Score;
+    totalPlayer2Score += player2Score;
+    const totalPlayerScore = document.querySelector('#player-score');
+    const totalComputerScore = document.querySelector('#computer-score');
+    totalPlayerScore.innerText = `Player: ${totalPlayer1Score}`;
+    totalComputerScore.innerText = `Computer: ${totalPlayer2Score}`;
 }
 
-playGame();
-//The game works
-//But it keeps propmting the user for their selection 
-//without being shown the results on the last round before being prompted to start another round
-//Player should have the option to start the
+function checkWinner() {
+    const finalResultsDiv = document.querySelector('#final-results');
+        //finalResultsDiv kept pushing the other divs down after showing results
+        //placed a placeholder in index.html with ""
+        //the placeholder was invisble but took space 
+        //to avoid the divs being pushed down
+        if (totalPlayer1Score >= winningScore) {
+            const resetConfirmation = confirm("Player Wins! Do you want to play another game?");
+            if (resetConfirmation) {
+                resetGame();
+            }
+            else {
+                finalResultsDiv.innerText = "Player Wins!";
+            }
+        } else if (totalPlayer2Score >= winningScore) {""
+            const resetConfirmation = confirm("Computer Wins! Do you want to play another game?");
+            if (resetConfirmation) {
+                resetGame();
+            }
+            else {
+                finalResultsDiv.innerText = "Computer Wins!";
+            }
+        }
+    }
+
+// Initial update of total scores
+updateScores();
+
+function resetGame() {
+    totalPlayer1Score = 0;
+    totalPlayer2Score = 0;
+    const totalPlayerScore = document.querySelector('#player-score');
+    const totalComputerScore = document.querySelector('#computer-score');
+    totalPlayerScore.innerText = `Player: ${totalPlayer1Score}`;
+    totalComputerScore.innerText = `Computer: ${totalPlayer2Score}`;
+    const finalResultsDiv = document.querySelector('#final-results');
+    finalResultsDiv.style.visibility = "hidden";
+    finalResultsDiv.innerText = "";
+}
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        let playerSelection;
+        switch (button.id) {
+            case 'rock':
+                playerSelection = getPlayerChoice('rock');
+                break;
+            case 'paper':
+                playerSelection = getPlayerChoice('paper');
+                break;
+            case 'scissors':
+                playerSelection = getPlayerChoice('scissors');
+                break;
+        }
+        const [player1Score, player2Score] = playRound(playerSelection);
+        updateScores(player1Score, player2Score);
+        checkWinner();
+    });
+});
